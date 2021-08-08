17dcc0d21b7e18b573a514f51c34509e5c514a5a
iriasmatheus <55512225+iriasmatheus@users.noreply.github.com>
2021-08-07 23:27:22 -0300

    Revert "Commit 3"
    This reverts commit c7a9c36aa0d44d817202a45e87c3d5873a15982a.

--- Customer.java
+++ Customer.java
@@ -46,7 +46,7 @@ public class Customer {
      return result;
    }
 
-   private double amountFor(Rental aRental) {
+   private double amountFor(Rental each) {
       switch (each.getMovie().getPriceCode()) {
          case Movie.REGULAR:
             thisAmount += 2;
